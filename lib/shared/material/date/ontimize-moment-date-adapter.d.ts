087ad6af7c8d50fe03a6ba60b6c578b6fbd217a4
Moisés Gramary <moisesg@marosavat.com>
2021-02-25 17:09:13 +0100

    Fix moment importing

--- lib/shared/material/date/ontimize-moment-date-adapter.d.ts
+++ lib/shared/material/date/ontimize-moment-date-adapter.d.ts
@@ -1,9 +1,8 @@
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
-import { Moment } from 'moment';
 export declare class OntimizeMomentDateAdapter extends MomentDateAdapter {
     oFormat: string;
     constructor(dateLocale: string);
     format(date: any, displayFormat: string): string;
     parse(value: any, parseFormat: string | string[]): any | null;
-    deserialize(value: any): Moment | null;
+    deserialize(value: any): any | null;
 }
